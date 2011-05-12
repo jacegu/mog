a018a163e4ca56b61fe163227cb27dbe2482a541
Javier Acero <j4cegu@gmail.com>
2011-05-12 20:48:43 +0200

    accessing configuration from the blog

--- lib/mog/blog.rb
+++ lib/mog/blog.rb
@@ -1,8 +1,27 @@
 module Mog
   class Blog
     attr_reader :config
+
     def initialize(configuration)
       @config = configuration
+      define_readers_for_each_config_option
+    end
+
+    private
+
+    def define_readers_for_each_config_option
+      @config.instance_variables.each do |var|
+        option = var.to_s.sub('@', '')
+        define_reader_for(option)
+      end
+    end
+
+    def define_reader_for(option)
+      Blog.class_eval do
+        define_method(option) do
+          eval "@config.#{option}"
+        end
+      end
     end
   end
 end
--- spec/mog/blog_spec.rb
+++ spec/mog/blog_spec.rb
@@ -7,5 +7,13 @@ module Mog
       blog = Blog.new(configuration)
       blog.config.should_not be_nil
     end
+
+    it 'can provide configuration values' do
+      the_configured_name = 'the name'
+      config = stub(:configuration)
+      config.stub(:name).and_return(the_configured_name)
+      blog =Blog.new(config)
+      blog.name.should == the_configured_name
+    end
   end
 end
