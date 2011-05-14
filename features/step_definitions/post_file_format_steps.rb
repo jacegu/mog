29c2bd269e2eb888ebf220245a86989ec7adade8
Javier Acero <j4cegu@gmail.com>
2011-05-14 11:55:43 +0200

    creating post with content step

---
+++ features/step_definitions/post_file_format_steps.rb
@@ -0,0 +1,20 @@
+Given /^a post file with the content:$/ do |file_content|
+  file_path = File.join(Dir.tmpdir, 'cucumber_post.post.html')
+  @file = File.open(file_path, 'w+'){ |f| f.puts(file_content) }
+end
+
+When /^a post is created from that file$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the post title should be "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the post description should be "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the post content should contain "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
