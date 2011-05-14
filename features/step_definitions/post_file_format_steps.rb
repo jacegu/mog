d0c7ccd24408ab8a2260614afa132f1da1330743
Javier Acero <j4cegu@gmail.com>
2011-05-14 17:59:40 +0200

    post file and parse title step

--- features/step_definitions/post_file_format_steps.rb
+++ features/step_definitions/post_file_format_steps.rb
@@ -1,15 +1,15 @@
 Given /^a post file with the content:$/ do |file_content|
   file_path = File.join(Dir.tmpdir, 'cucumber_post.post.html')
   file = File.open(file_path, 'w+'){ |f| f.puts(file_content) }
-  @post_file = Mog::PostFile.new(file)
+  @post_file = Mog::PostFile.new(file_path)
 end
 
 When /^a post is created from that file$/ do
   @post = Mog::Post.from_post_file(@post_file)
 end
 
-Then /^the post title should be "([^"]*)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then /^the post title should be "([^"]*)"$/ do |title|
+  @post.title.should == title
 end
 
 Then /^the post description should be "([^"]*)"$/ do |arg1|
--- lib/mog/post_file.rb
+++ lib/mog/post_file.rb
@@ -1,4 +1,33 @@
 module Mog
   class PostFile
+    attr_reader :source_file_content
+
+    FIRST_LINE = 0
+    SECOND_LINE = 1
+    THIRD_LINE = 2
+    LAST_LINE = -1
+
+    def initialize(path)
+      File.open(path, 'r'){ |f| @source_file_content = f.read }
+    end
+
+    def title
+      content_lines[FIRST_LINE]
+    end
+
+    def description
+      content_lines[SECOND_LINE]
+    end
+
+    def content
+      content_lines[THIRD_LINE..LAST_LINE].join("\n")
+    end
+
+    def content_lines
+      lines = @source_file_content.split("\n")
+      lines_without_trail_spaces = lines.map{ |line| line.lstrip }.compact
+      lines_non_empty = lines_without_trail_spaces.select{ |l| not l.empty? }
+    end
+
   end
 end
