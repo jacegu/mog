2047f316bc30d4a360e3ec8b76ee850f7aa1f2bc
Javier Acero <j4cegu@gmail.com>
2011-06-22 12:50:23 +0200

    posting from a dir with post files and non post files

--- features/post_from_directories.feature
+++ features/post_from_directories.feature
@@ -4,12 +4,16 @@
 Feature: Posting from a directory
 
   Scenario: A single post file in the directory
+
     Given I have a published post in the post directory
      When I configure the blog with that directory
      Then the post should appear in the blog
 
+
   Scenario: A directory with a post file and a non-post file
+
     Given I have a published post in the post directory
       And there is a file that is not a post in the directory
      When I configure the blog with that directory
      Then the post should appear in the blog
+      And the blog should have only 1 post
--- features/step_definitions/post_from_directories_steps.rb
+++ features/step_definitions/post_from_directories_steps.rb
@@ -1,15 +1,22 @@
+TEMPORAL_DIR = Dir.tmpdir
+
+def create_file_at_temporal_dir(name, content)
+  file_path = File.join(TEMPORAL_DIR, name)
+  File.open(file_path, 'w+'){ |f| f.puts content }
+end
+
 Given /^I have a published post in the post directory$/ do
   @post = Mog::Post.new('title', 'description', 'content', DateTime.parse('2011-05-08 10:00'))
-  @dir_path = Dir.tmpdir
-  file_path = File.join(Dir.tmpdir, 'cucumber_post.post.html')
-  File.open(file_path, 'w+') do |f|
-    f.puts "#{@post.publication_time}\n#{@post.title}\n#{@post.description}\n#{@post.content}"
-  end
+  create_file_at_temporal_dir 'cucumber_post.post.html',
+    "#{@post.publication_time}\n#{@post.title}\n#{@post.description}\n#{@post.content}"
+end
+
+Given /^there is a file that is not a post in the directory$/ do
+  create_file_at_temporal_dir 'not_a_post.html', 'this is no t a post file'
 end
 
 When /^I configure the blog with that directory$/ do
-  dir = Dir.open(@dir_path)
-  post_directory = Mog::PostDir.from(dir)
+  post_directory = Mog::PostDir.at(TEMPORAL_DIR)
   config = Mog::Configuration.new
   config.set(:locations, [post_directory])
   @blog = Mog::Blog.new(config)
@@ -17,4 +24,8 @@ end
 
 Then /^the post should appear in the blog$/ do
   @blog.posts.should include(@post)
+end
+
+Then /^the blog should have only ([^"]*) post$/ do |expected_number_of_posts|
+  @blog.posts.count.should == expected_number_of_posts.to_i
 end
